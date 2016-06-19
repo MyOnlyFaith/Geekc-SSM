b49d004e9ae9ae8afc6f873339169c420462dc04
geekc <291438032@qq.com>
2016-06-19 22:35:46 +0800

    test demo

--- geekc-ssm/src/main/java/cn/geekc/ssm/webapp/controller/UserController.java
+++ geekc-ssm/src/main/java/cn/geekc/ssm/webapp/controller/UserController.java
@@ -1,27 +1,29 @@
 package cn.geekc.ssm.webapp.controller;
 
-
-
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
-import cn.geekc.ssm.model.User;
 import cn.geekc.ssm.service.UserService;
 
 @Controller
-@RequestMapping(value="/user")
+@RequestMapping(value = "/user")
 public class UserController {
-	
+
 	@Autowired
 	private UserService userService;
-	
-	@RequestMapping(value="/getuserbyid")
+
+	@RequestMapping(value = "/getuserbyid")
 	@ResponseBody
-	public User getUserById(@RequestParam(value="userId",required = true)Long userId){
-		return userService.getUserById(userId);
+	public ModelAndView getUserById(@RequestParam(value = "userId", required = true) Long userId,
+			HttpServletRequest request) {
+		ModelAndView modelAndView = new ModelAndView("/test");
+		modelAndView.addObject("user", userService.getUserById(userId));
+		return modelAndView;
 	}
 }
